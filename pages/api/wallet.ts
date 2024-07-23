71fbfd222d79e0620c542bf7573b709e7f889f20
neosa <m5557730@yandex.ru>
2024-07-24 01:10:36 +0300

    add wallet page

--- pages/api/wallet.ts
+++ pages/api/wallet.ts
@@ -6,15 +6,15 @@ const router = new RouterBuilder();
 router
 	.get(async (req) => {
 		const {id} = req.query
-		return (await axios.get(`${process.env.BOT_API}/api/airdrop`, {params: {id}})).data
+		return (await axios.get(`${process.env.BOT_API}/api/wallet`, {params: {id}})).data
 	})
 	.post(async (req) => {
 		const {walletAddress, id} = req.body
-		return (await axios.post(`${process.env.BOT_API}/api/airdrop`, {walletAddress, id})).data
+		return (await axios.post(`${process.env.BOT_API}/api/wallet`, {walletAddress, id})).data
 	})
 	.delete(async (req) => {
 		const {walletAddress, id} = req.query
-		return (await axios.delete(`${process.env.BOT_API}/api/airdrop`, {params: {id, walletAddress}})).data
+		return (await axios.delete(`${process.env.BOT_API}/api/wallet`, {params: {id, walletAddress}})).data
 	})
 
 export default router.build();
